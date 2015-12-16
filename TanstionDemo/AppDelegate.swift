1ec6b7c1efb998dd5d6457aca478df6957733409
zmjios <zmjios@mjzeng.cn1.global.ctrip.com>
2015-12-16 15:11:37 +0800

    VCTranstionDemo
    a ios7 Vctransition demo write by swift

--- TanstionDemo/AppDelegate.swift
+++ TanstionDemo/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let viewController:ViewController = ViewController.init()
+        let navigationContoller:UINavigationController = UINavigationController.init(rootViewController: viewController)
+        
+        self.window = UIWindow.init(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = navigationContoller
+        
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
